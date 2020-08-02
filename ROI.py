e8ba73d3bfb6df6b2544975b9fbe105a611a32be
Nibras M <nawaraaa2000@yahoo.com>
2020-08-02 09:44:33 -0700

    classwork

--- ROI.py
+++ ROI.py
@@ -18,20 +18,27 @@ class ROInvestment:
         self.capex = capex
         self.property_manag = property_manag
         self.mortage = mortage
-        self.expenses = tax + insurance + utilities + HOA + lawn_snow + vacancy + repairs + capex + property_manag + mortage
+        self.total_expenses = tax + insurance + utilities + HOA + lawn_snow + vacancy + repairs + capex + property_manag + mortage
 
 
-    def calculate_total_cash_flow(self, total_monthly_expenses):
-    
-        self.total_monthly_expenses = total_monthly_expenses
-        self.total_cash_flow = self.income - self.expenses
+    def calc_cash_flow(self):
+        self.total_cashflow = self.income - self.total_expenses
 
-
-    def cash_on_cashROI(self,down_payment,closing_costs,rehab_budget,misc):
+    def cash_on_cashROI(self,down_payment,closing_costs,rehab_budget,misc1):
         self.down_payment = down_payment
         self.closing_costs = closing_costs
         self.rehab_budget = rehab_budget
-        self.misc = misc
-        return (down_payment + closing_costs + rehab_budget + misc) / self.total_cash_flow
+        self.misc1 = misc1
+        return (down_payment + closing_costs + rehab_budget + misc1) / self.total_cashflow
+
+
+total_roinvestment = ROInvestment()  
+total_roinvestment.calc_income(30000,700,350,100)
+total_roinvestment.expenses(2000,120,450,300,120,140,230,50,60,1200)
+total_roinvestment.calc_cash_flow()
+final_ROI = total_roinvestment.cash_on_cashROI(20000,200,39,20)
+print('{:.1%}'.format(final_ROI))
+
+
 
     
